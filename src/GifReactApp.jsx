079d2f2172e4120fcece953829d4915bdd309fda
GaruCH <146020588+GaruCH@users.noreply.github.com>
2025-03-03 18:10:51 -0600

    Primeros pasos

---
+++ src/GifReactApp.jsx
@@ -0,0 +1,13 @@
+export const GifReactApp = () => {
+  return (
+    <div class="mt-5 ml-5">
+    {/* titulo */}
+      <h1 class="font-semibold font-primary text-2xl">GifReactApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gif */}
+      {/* Gif Item */}
+    </div>
+  );
+};
